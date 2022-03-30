a433d720af0a5736299b1bb7c50d64c8b2295b6f
Briyan A <briyana1997@gmail.com>
2022-03-30 18:16:38 -0400

    Tercer homework terminado xD

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,7 +2,7 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  const arreglo = num.toString().split("").reverse();
+  const arreglo = num.split("").reverse();
   let resultado = 0;
   for(var i = arreglo.length-1; i >= 0 ; i--){
       resultado += parseInt(arreglo[i]*(2**i));
@@ -13,12 +13,13 @@ function BinarioADecimal(num) {
 function DecimalABinario(num) {
   // tu codigo aca
   const residuos = [];
-  while(num != 0){
-      if(num % 2 === 0){
-          residuos.unshift(0);
-      }else{
-          residuos.unshift(1);
-      }
+  while(num){
+ /*      if(num % 2 === 0){
+        residuos.unshift(0);
+    }else{
+        residuos.unshift(1);
+    } */
+      residuos.unshift(num % 2);
       num = Math.floor(num / 2);
   }
   return residuos.join("");
